b4a07abd4cef7a22ed36737e65d32c5e39dd30c2
Jeonha Kang <kangjeonha@gmail.com>
2024-07-06 18:53:04 +0900

    sixth-commit

---
+++ src/component/BoxClass.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class BoxClass extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div className={`box ${this.props.result}`}>
+                <h1>{this.props.title}</h1>
+                <img className='item-img' src={this.props.item && this.props.item.img} />
+                <h2>{this.props.result}</h2>
+            </div>
+        )
+    }
+}
